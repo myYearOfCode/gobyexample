1d398a7aceea0b6876d45c3da1522313b47016cf
Ross <Ross@myyearofcode.com>
2019-06-22 21:04:42 -0400

    init commit

---
+++ 01_hello_world/hello_world.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
+// go run hello-world.go
+// runs without building
+
+// go build hello-world.go
+// builds it
+
+// ./hello-world
+// runs it
